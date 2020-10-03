7c4e65214b5023ec19ab8f41a0ea43db7cdfdc81
Mathijs Lemmers <mathijs750@gmail.com>
2020-10-03 14:33:11 +0200

    Add mesh and work on timer interval

--- game/scripts/BodyPart.gd
+++ game/scripts/BodyPart.gd
@@ -10,21 +10,36 @@ onready var body_part_progress = 20
 onready var is_active = true
 
 func _ready():
-	pass # Replace with function body.
+	$Mesh.scale = Vector3($Mesh.scale.x, body_part_progress / 100.0, $Mesh.scale.z)
 
 func increment_part():
-	body_part_progress += PROGRESS_VALUE
+	if (is_active):
+		body_part_progress += PROGRESS_VALUE
 
+
+func update_body_part():
+	if (is_active):
+		body_part_progress -= REGRESS_VALUE
+
+		if (body_part_progress >= 100):
+			emit_signal("body_part_complete")
+			is_active = false
+
+		if (body_part_progress <= 0):
+			emit_signal("body_part_failed")
+			is_active = false
+			$Mesh.visible = false
+
+	print("body_part_progress: ", body_part_progress)
+
+
+func _on_IncrementTimer_timeout():
+	update_body_part()
 
 func _process(delta):
-	body_part_progress -= REGRESS_VALUE
+	var last_scale = $Mesh.scale
+	var new_scale = Vector3(last_scale.x, body_part_progress / 100.0, last_scale.z)
+	$Mesh.scale = $Mesh.scale.linear_interpolate(new_scale, delta + 0.3)
 
-	if (body_part_progress >= 100):
-		emit_signal("body_part_complete")
-		is_active = false
-
-	if (body_part_progress <= 0):
-		emit_signal("body_part_failed")
-		is_active = false
-
-	print("body_part_progress: %f", body_part_progress)
+	if (Input.is_action_just_released("ui_accept")):
+		increment_part()
