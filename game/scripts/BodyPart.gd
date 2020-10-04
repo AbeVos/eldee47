f2a42979cb6ae8374d635764ea1d4f50c06e3292
Mathijs Lemmers <mathijs750@gmail.com>
2020-10-04 21:37:07 +0200

    Voeg scene change toe aan het einde van het spel

--- game/scripts/BodyPart.gd
+++ game/scripts/BodyPart.gd
@@ -13,34 +13,34 @@ onready var body_part_progress = 30
 onready var is_active = true
 
 func _ready():
-    transform.origin = hidden_pos
+	transform.origin = hidden_pos
 
 func increment_part():
-    if (is_active):
-        body_part_progress += PROGRESS_VALUE
+	if (is_active):
+		body_part_progress += PROGRESS_VALUE
 
 
 func update_body_part():
-    if (is_active):
-        body_part_progress -= REGRESS_VALUE
+	if (is_active):
+		body_part_progress -= REGRESS_VALUE
 
-        if (body_part_progress >= 100):
-            emit_signal("body_part_complete")
-            is_active = false
+		if (body_part_progress >= 100):
+			emit_signal("body_part_complete")
+			is_active = false
 
-        if (body_part_progress <= 0):
-            emit_signal("body_part_failed")
-            is_active = false
-            visible = false
+		if (body_part_progress <= 0):
+			emit_signal("body_part_failed")
+			is_active = false
+			visible = false
 
-    #print("body_part_progress: ", body_part_progress)
+	#print("body_part_progress: ", body_part_progress)
 
 
 func _on_IncrementTimer_timeout():
-    update_body_part()
+	update_body_part()
 
 func _process(_delta):
-    # var last_scale = $Mesh.scale
-    # var new_scale = Vector3(last_scale.x, body_part_progress / 100.0, last_scale.z)
-    # $Mesh.scale = $Mesh.scale.linear_interpolate(new_scale, delta + 0.3)
-    transform.origin = hidden_pos.linear_interpolate(visible_pos, body_part_progress/100.0)
+	# var last_scale = $Mesh.scale
+	# var new_scale = Vector3(last_scale.x, body_part_progress / 100.0, last_scale.z)
+	# $Mesh.scale = $Mesh.scale.linear_interpolate(new_scale, delta + 0.3)
+	transform.origin = hidden_pos.linear_interpolate(visible_pos, body_part_progress/100.0)
--- game/scripts/Monster.gd
+++ game/scripts/Monster.gd
@@ -9,64 +9,65 @@ onready var results = []
 onready var is_active = true
 
 func _on_Choir_send_assignments(group_key, assignment):
-    print("Group key: ", group_key, " Assignment: ", assignment)
+	print("Group key: ", group_key, " Assignment: ", assignment)
 
-    var body_parts = [
-        $Head, $Claw_left, $Claw_right,
-    ]
+	var body_parts = [
+		$Head, $Claw_left, $Claw_right,
+	]
 
-    for cultist_idx in assignment:
-        if cultist_idx == null:
-            continue
+	for cultist_idx in assignment:
+		if cultist_idx == null:
+			continue
 
-        emit_signal("set_target", cultist_idx, body_parts[group_key])
+		emit_signal("set_target", cultist_idx, body_parts[group_key])
 
-    if not null in assignment:
-        if (is_active):
-            body_parts[group_key].increment_part()
+	if not null in assignment:
+		if (is_active):
+			body_parts[group_key].increment_part()
 
 
-func _process(delta):
-    if (is_active and results.size() >= 3):
-        is_active = false
-        var total = 0
-        for i in range(results.size()):
-            total += i
-        if (total >= results.size()):
-            emit_signal("monster_done")
-        elif (total > 0):
-            emit_signal("monster_partial")
-        else:
-            emit_signal("monster_failed")
-
-        print("We zijn klaar met een totaal van: ", total)
+func _process(_delta):
+	if (is_active and results.size() >= 3):
+		is_active = false
+		var total = 0
+		for i in range(results.size()):
+			total += i
+		if (total >= results.size()):
+			emit_signal("monster_done")
+			SceneChanger.change_scene("res://scenes/WinScene.tscn", 0.5)
+		elif (total > 0):
+			emit_signal("monster_partial")
+			SceneChanger.change_scene("res://scenes/FailScene.tscn", 0.5)
+		else:
+			emit_signal("monster_failed")
+			SceneChanger.change_scene("res://scenes/FailScene.tscn", 0.5)
 
 
 func _on_IncrementTimer_timeout():
-    $Head.update_body_part()
-    $Claw_left.update_body_part()
-    $Claw_right.update_body_part()
+	$Head.update_body_part()
+	$Claw_left.update_body_part()
+	$Claw_right.update_body_part()
 
 
 func _on_Head_body_part_failed():
-    results.append(0)
+	results.append(0)
 
 
 func _on_Claw_left_body_part_failed():
-    results.append(0)
+	results.append(0)
 
 
 func _on_Claw_right_body_part_failed():
-    results.append(0)
+	results.append(0)
 
 
 func _on_Head_body_part_complete():
-    results.append(1)
+	results.append(1)
 
 
 func _on_Claw_left_body_part_complete():
-    results.append(1)
+	results.append(1)
 
 
 func _on_Claw_right_body_part_complete():
-    results.append(1)
+	results.append(1)
