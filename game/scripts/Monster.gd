1f04b1c8c4ed3381b6e8f1b84670c30762005181
Mathijs Lemmers <mathijs750@gmail.com>
2020-10-04 14:09:47 +0200

    Zorg dat lichaamsdelen omhoog gaan met een akoord

--- game/scripts/Monster.gd
+++ game/scripts/Monster.gd
@@ -8,47 +8,67 @@ onready var results = []
 onready var is_active = true
 
 func _on_Choir_send_assignments(group_key, assignment):
-	pass
-	#print("Group key: ", group_key, " Assignment: ", assignment)
+    print("Group key: ", group_key, " Assignment: ", assignment)
+    var total 
+    match group_key:
+        1:
+            for i in range(3):
+
+                if (assignment[i] == null):
+                    return
+            
+            $Head.increment_part()
+        2:
+            for i in range(3):
+                if (assignment[i] == null):
+                    return
+            
+            $Claw_left.increment_part()
+        3:
+            for i in range(3):
+                if (assignment[i] == null):
+                    return
+            
+            $Claw_right.increment_part()
 
 func _process(delta):
-	if (is_active and results.size() >= 3):
-		is_active = false
-		var total = 0
-		for i in range(results.size()):
-			total += i
-		if (total >= results.size()):
-			emit_signal("monster_done")
-		elif (total > 0):
-			emit_signal("monster_partial")
-		else: 
-			emit_signal("monster_failed")
+    if (is_active and results.size() >= 3):
+        is_active = false
+        var total = 0
+        for i in range(results.size()):
+            total += i
+        if (total >= results.size()):
+            emit_signal("monster_done")
+        elif (total > 0):
+            emit_signal("monster_partial")
+        else: 
+            emit_signal("monster_failed")
 
-		print("We zijn klaar met een totaal van: ", total)
+        print("We zijn klaar met een totaal van: ", total)
 
 
 func _on_IncrementTimer_timeout():
-	#$Head.update_body_part()
-	#$Claw_left.update_body_part()
-	#$Claw_right.update_body_part()
-	pass
+    #$Head.update_body_part()
+    #$Claw_left.update_body_part()
+    #$Claw_right.update_body_part()
+    pass
 
 
 func _on_Head_body_part_failed():
-	results.append(0)
+    results.append(0)
 
 func _on_Claw_left_body_part_failed():
-	results.append(0)
+    results.append(0)
 
 func _on_Claw_right_body_part_failed():
-	results.append(0)
+    results.append(0)
 
 
 func _on_Head_body_part_complete():
-	results.append(1)
-	
+    results.append(1)
+    
 func _on_Claw_left_body_part_complete():
-	results.append(1)
+    results.append(1)
 
 func _on_Claw_right_body_part_complete():
-	results.append(1)
+    results.append(1)
